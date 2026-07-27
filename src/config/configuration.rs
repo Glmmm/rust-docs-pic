5273e4c507fbad9f48098a534c63a6676cad9adc
Guilherme Papa <64666460+Glmmm@users.noreply.github.com>
2026-07-27 16:47:23 -0300

    feat: adiciona nivel de produndidade para listagem de arquivos

--- src/config/configuration.rs
+++ src/config/configuration.rs
@@ -1,28 +1,25 @@
 use serde::Deserialize;
 use std::fs;
 
-use crate::log;
-
 #[derive(Deserialize, Debug)]
 pub struct Configuration {
     pub input_path: String,
     pub output_path: String,
     pub template_path: String,
     pub exclude: Vec<String>,
-    pub recursive: bool,
+    pub depth: usize,
     pub include_hidden: bool,
     pub verbose: bool,
 }
 //TODO: implementar tela de configuração
 impl Configuration {
     pub fn default() -> Self {
-        log::info("Iniciando com a configuração padrão");
         return Self {
             input_path: "./src".into(),
             output_path: "./docs".into(),
             template_path: "../template.md".into(),
             exclude: vec![],
-            recursive: false,
+            depth: 5,
             include_hidden: false,
             verbose: true,
         };
--- src/parser/files.rs
+++ src/parser/files.rs
@@ -1,21 +1,22 @@
 use std::path::PathBuf;
+
 use walkdir::WalkDir;
 
-use crate::{config::Configuration, log};
+use crate::config::Configuration;
 
 pub fn find_files(config: &Configuration) -> Vec<PathBuf> {
     return WalkDir::new(&config.input_path)
+        .max_depth(config.depth)
         .into_iter()
         .filter_map(|e| e.ok())
         .map(|e| e.path().to_path_buf())
-        .filter(|path| !is_excluded(path, &config.exclude))
+        .filter(|path| !is_excluded(path, &config))
         .collect();
 }
 
-fn is_excluded(path: &PathBuf, excluded: &[String]) -> bool {
-    for dir in excluded {
+fn is_excluded(path: &PathBuf, config: &Configuration) -> bool {
+    for dir in &config.exclude {
         if path.components().any(|c| c.as_os_str() == dir.as_str()) {
-            log::info(&format!("Analisando arquivo {:?}", path));
             return true;
         }
     }
